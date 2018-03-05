444f60954f6cff0fec1b205d5e88a42496dc2c9e
vidisha <vidisha.gupta@knoldus.in>
2018-03-05 13:28:51 +0530

    Modified MoviesOperations File

--- src/edu/knoldus/services/MovieOperations.java
+++ src/edu/knoldus/services/MovieOperations.java
@@ -7,7 +7,7 @@ import java.util.List;
 
 public class MovieOperations {
 
-    public List<Movie> makeMovieList() {
+    private List<Movie> makeMovieList() {
         List<Movie> movieList = new ArrayList<>();
         movieList.add(new Movie("Titanic", 1997, 9, "Romance"));
         movieList.add(new Movie("Golmaal", 2006, 8, "Comedy"));
@@ -22,30 +22,30 @@ public class MovieOperations {
 
     public void performOperationsOnMovieList() {
         List<Movie> moviesList = makeMovieList();
-        /* 1: List out all the movies with rating more than 8 and genre comedy.*/
+        /* List out all the movies with rating more than 8 and genre comedy.*/
         System.out.println("Movie with rating more than 8 and Genre is Comedy = ");
         moviesList.stream()
                 .filter(search -> search.getRatings() >= 8 && search.getGenre()
                         .equals("Comedy"))
-                .map(searchMovie -> searchMovie.getName()).forEach(System.out::println);
+                .map(Movie::getName).forEach(System.out::println);
 
-        /* 2: List out all the movies with release year greater than 2000 and rating less than 8.*/
+        /* List out all the movies with release year greater than 2000 and rating less than 8.*/
         System.out.println("Movie With Rating less than 8 and rating is less than 8 = ");
         moviesList.stream()
                 .filter(search -> search.getReleasedYear() > 2000 && search.getRatings() < 8)
-                .map(searchMovie -> searchMovie.getName()).forEach(System.out::println);
+                .map(Movie::getName).forEach(System.out::println);
 
-        /* 3: List out all the movies with rating as even number.*/
+        /* List out all the movies with rating as even number.*/
         System.out.println("Movies with  rating in even number = ");
         moviesList.stream()
                 .filter(search -> search.getRatings() % 2 == 0)
-                .map(searchMovie -> searchMovie.getName()).forEach(System.out::println);
+                .map(Movie::getName).forEach(System.out::println);
 
-        /* 4: List out all the movies with rating equals to 7 and genre Sci-Fi.*/
+        /* List out all the movies with rating equals to 7 and genre Sci-Fi.*/
         System.out.println("Movies With rating equal to 7 and genre Sci- Fi = ");
         moviesList.stream()
                 .filter(search -> search.getRatings() == 7 && search.getGenre()
                         .equals("Sci-fi"))
-                .map(searchMovie -> searchMovie.getName()).forEach(System.out::println);
+                .map(Movie::getName).forEach(System.out::println);
     }
 }
